13e854fbbdabf7027be2f7d7316880e16b8ad0c9
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-02-28 14:55:51 +0000

    fixing the netlify problem

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
 
 const average = (arr) =>
-  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+  arr?.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 //
 //  const KEYS = [ '4b447405',
