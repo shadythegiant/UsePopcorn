5fb021e501a01a3d4e4d1cc8ed07e2e43b71a676
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-02-28 19:31:35 +0000

    trying again  :octocat:

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
+
 export function useLocalStorage(initialState, key) {
   const [value, setValue] = useState(function () {
-    const sotredValue = localStorage.getItem(key);
-    return sotredValue ? JSON.parse(sotredValue) : initialState;
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialState;
   });
 
-  // local Storage effect
-
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [value, key]);
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    [value, key]
+  );
 
   return [value, setValue];
 }
